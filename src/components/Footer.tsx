c501163c038c89e91f8a5778a684e32d1e461a9d
attiredoc-art <attiredoc@gmail.com>
2025-10-18 21:41:49 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -92,9 +92,8 @@ const Footer: React.FC = () => {
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center">
           <p className="text-gray-400">
-            © {new Date().getFullYear()} MAIPA. All rights reserved.
+            © {new Date().getFullYear()} MAIPA. All rights reserved. <span><a href="/admin/dashboard">...</a></span>
           </p>
-          <a href="/admin/dashboard">...</a>
         </div>
       </div>
     </footer>
